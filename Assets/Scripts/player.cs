3b5d508bbace14675f7ef0066c0c36d0681f990b
corri.moran <moranc3@hawkmail.newpaltz.edu>
2017-03-27 10:46:09 -0400

    Added player script

---
+++ Assets/Scripts/player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour {
+
+	public int health = 100;
+
+	new Rigidbody2D rigidbody; 
+
+	// Use this for initialization
+	void Start () {
+		rigidbody = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float x = Input.GetAxisRaw ("Horizontal");
+		rigidbody.velocity = new Vector2 (x, 0);
+	}
+}
