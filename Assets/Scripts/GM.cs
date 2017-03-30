0aed9aee411d211c7722f4ba362222fdfe2cb3a1
corri.moran <moranc3@hawkmail.newpaltz.edu>
2017-03-30 10:48:40 -0400

    Edited Scripts

--- Assets/Scripts/GM.cs
+++ Assets/Scripts/GM.cs
@@ -1,16 +1,10 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class GM : MonoBehaviour {
+	public int lives; 
+	public int points;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	public void SetLives(int newValue) {
+		lives = newValue;
 	}
 }
--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -5,17 +5,46 @@ using UnityEngine;
 public class player : MonoBehaviour {
 
 	public int health = 100;
+	public float speed = 5;
+	public float jumpSpeed = 5;
+	public float deadZone = -4;
 
 	new Rigidbody2D rigidbody; 
+	GM _GM; 
+
 
 	// Use this for initialization
 	void Start () {
 		rigidbody = GetComponent<Rigidbody2D> ();
+		_GM = FindObjectOfType<GM> ();
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
+
+		// Apply Movement
 		float x = Input.GetAxisRaw ("Horizontal");
-		rigidbody.velocity = new Vector2 (x, 0);
+		Vector2 v = rigidbody.velocity;
+		v.x = x * speed;
+
+		if (Input.GetButtonDown ("Jump")) {
+			v.y = jumpSpeed;
+		}
+
+		rigidbody.velocity = v;
+
+		if (transform.position.y < deadZone) {
+			Debug.Log ("You're Out");
+		}
+
+
+		//rigidbody.velocity = new Vector2 (x * speed , rigidbody.velocity.y);
+
+
+		//rigidbody.AddForce (new Vector2 (x * speed, 0));
+	}
+
+	public void GetOut() {
+		_GM.SetLives (_GM.lives - 1);
 	}
 }
